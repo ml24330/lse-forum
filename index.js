4e36a6f1940d79506d31e1fef05a1b2e2eda70ed
ml <m.liu48@lse.ac.uk>
2021-02-26 09:02:46 +0000

    revert middleware

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use((req, res, next) => {
-    if (!req.secure && process.env.NODE_ENV === 'production') {
-        res.redirect('https://' + req.get('Host') + req.url);
-    } else
-        next();
-});
+// app.use((req, res, next) => {
+//     if (!req.secure && process.env.NODE_ENV === 'production') {
+//         res.redirect('https://' + req.get('Host') + req.url);
+//     } else
+//         next();
+// });
 
 app.use(express.static(path.resolve(__dirname, "build")));
 
