bf2fb10429c03bb883eab92faf29c010a9003bb2
ml <m.liu48@lse.ac.uk>
2021-02-26 09:01:28 +0000

    add middleware

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-const middleWare = (req, res, next) => {
-    if (!req.secure) {
+app.use((req, res, next) => {
+    if (!req.secure && process.env.NODE_ENV === 'production') {
         res.redirect('https://' + req.get('Host') + req.url);
     } else
         next();
-    };
+});
 
 app.use(express.static(path.resolve(__dirname, "build")));
 
-app.get("*", middleWare, (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
+
+
+
+
 
 app.listen(PORT, console.log(`Started listening on port ${PORT}`));
