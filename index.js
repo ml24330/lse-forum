3b74500f84212891e42f630a4beb70b5ffc77896
ml <m.liu48@lse.ac.uk>
2021-02-26 09:41:29 +0000

    test middleware

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.resolve(__dirname, "build")));
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
